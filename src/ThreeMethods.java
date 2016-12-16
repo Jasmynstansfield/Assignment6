c1089f7d7a8d2e75dd44aa0c3038a495c9d642c8
Jazz <jasmynpaige14@gmail.com>
2016-12-16 15:52:20 -0600

    Assignment 6, program 1
    display name, birthday, fact

---
+++ src/ThreeMethods.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+
+/**
+ * ThreeMethods.java
+ * Jasmyn Stansfield
+ * Mr. Hardman
+ * Assignment 6, program 1
+ * December 8, 2016
+ */
+public class ThreeMethods {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		displayName();
+		displayBirthday();
+		displayFact();
+		
+	}
+	
+	/**
+	 * displayName will display will display my name
+	 * to the user
+	 * 
+	 * @param there are no parameters
+	 * @return nothing will be returned
+	 */
+	public static void displayName(){
+		
+		System.out.println("Jasmyn Stansfield");
+		
+	}
+	
+	/**
+	 * displayBirthday will display will display my
+	 * birthday to the user
+	 * 
+	 * @param there are no parameters
+	 * @return nothing will be returned
+	 */
+	public static void displayBirthday(){
+		
+		System.out.println("June 8, 2001");
+		
+	}
+	
+	/**
+	 * displayFact will display will display a fact 
+	 * about me to the user
+	 * 
+	 * @param there are no parameters
+	 * @return nothing will be returned
+	 */
+	public static void displayFact(){    
+		
+		System.out.println("My mom and I share the same birthday");  
+		
+	}
+
+}
